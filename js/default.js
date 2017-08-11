da389398c682cbdc789078590f1825c7662ad8c7
mmmmmj <iam924mj@gmail.com>
2017-08-11 16:40:28 +0900

    guest

--- js/default.js
+++ js/default.js
@@ -1,46 +1,83 @@
 $(function()
 {
-	setTimeout(function()
+	$(document).ready(function()
 	{
-		$(window).resize(function()
+		setTimeout(function()
 		{
-			if ($(window).width() >= ($(window).height() * 1.4))
+			$(window).resize(function()
 			{
-				$('#main').addClass('web');
-			}		
-			else
-			{
-				$('#main').removeClass('web');
-				$('#main').find('.img > *').css('marginLeft','-' + $('#main').find('.img > *').width() / 2 + 'px');
-			}
-
-		}).resize();
-
-		$('#menu').find('a').on('click', function()
-		{
-			if ($(this).parent('li').hasClass('close') == false)
-			{
-				if ($(this).attr('href') == '#main')
+				if ($(window).width() >= ($(window).height() * 1.4))
 				{
-					$('#menu').find('li').removeClass('close');
-					$('section').removeClass('on');
+					$('#main').addClass('web');
+				}		
+				else
+				{
+					$('#main').removeClass('web');
+					$('#main').find('.img > *').css('marginLeft','-' + $('#main').find('.img > *').width() / 2 + 'px');
+				}
+
+			}).resize();
+
+			$('#menu').find('a').on('click', function()
+			{
+				if ($(this).parent('li').hasClass('close') == false)
+				{
+					if ($(this).attr('href') == '#main')
+					{
+						$('#menu').find('li').removeClass('close');
+						$('section').removeClass('on');
+					}
+					else
+					{
+						$('section').removeClass('on');
+						$(this.hash).addClass('on');
+
+						$('#menu').find('li').removeClass('close');
+						$(this).parent('li').addClass('close');
+					}
 				}
 				else
 				{
-					$('section').removeClass('on');
-					$(this.hash).addClass('on');
-
-					$('#menu').find('li').removeClass('close');
-					$(this).parent('li').addClass('close');
+					$(this.hash).removeClass('on');
+					$(this).parent('li').removeClass('close');
 				}
-			}
-			else
+
+				return false;
+			});
+		}, 200);
+
+		$.ajax
+		({
+			type		: 'POST',
+			url			: './m2_request.php',
+			dataType	: 'html',
+			success		: function (data)
 			{
-				$(this.hash).removeClass('on');
-				$(this).parent('li').removeClass('close');
+				$('ul#add').append(data);
+
 			}
+		});
+
+		function ajax_ok()
+		{
+			var params="memo="+$('#memo').val();  
+
+			$.ajax
+			({
+				type		: 'POST',
+				url			: './m2_request.php',
+				data		: params,
+				dataType	: 'html',
+				success		: function (data)
+				{
+					$('ul#add').html('');
+					$('ul#add').append(data);
+
+				}
+			});
 
 			return false;
-		});
-	}, 400);
+		}
+
+	});
 });
