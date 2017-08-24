457ec10a64962efe642a30c83c7adb64ba83779a
mmmmmj <iam924mj@gmail.com>
2017-08-24 17:47:35 +0900

    ho

--- js/guest.js
+++ js/guest.js
@@ -1,5 +1,12 @@
 $(function()
 {
 	$('.text1 > tbody').find('> tr:last').remove();
-	$('.text1 > tbody').find('> tr:nth-last-child(2)').remove();
+
+	$('.text1 > tbody > tr').each(function(i)
+	{
+		if ($(this).find('select.text').length == 1)
+		{
+			$('.text1 > tbody > tr:eq(i)').remove();
+		}
+	});
 });
